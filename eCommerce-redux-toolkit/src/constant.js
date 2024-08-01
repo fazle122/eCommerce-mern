6a9e7ba015041e71a8a01ff1c24356bbdfc364f8
fr <fazlerabby122@gmail.com>
2024-08-01 10:23:07 +0600

    first-commit

---
+++ eCommerce-redux-toolkit/src/constant.js
@@ -0,0 +1,16 @@
+
+// export const BASE_URL = "http://localhost:8000";
+export const BASE_URL = "";
+// export const PRODUCTS_URL = 'http://localhost:3000/products'; //// for local data
+export const PRODUCTS_URL = '/api/products';
+export const USERS_URL = '/api/users';
+export const ORDERS_URL = '/api/orders';
+export const PAYPAL_URL = '/api/config/paypal';
+export const UPLOAD_URL = '/api/upload';
+
+export const PRODUCT_CATEGORIES =[
+    "Accessories",
+    "Mobile",
+    "Camera",
+    "Electronics",
+]
---
+++ eCommerce-redux-toolkit/src/products.js
@@ -0,0 +1,40 @@
+const  products = [
+    {
+        _id:1,
+        name:'abc abc abc abc abc',
+        image:'images/airpods.jpg',
+        description:'afdadfa adfa dfoqf jad fapddfaj;dfapdfjapdf apddfj adpfdajsdfp asdpdfbapdodf asdpfouaopbbjdfadfd aosdfubapodfjapdjfdopajfdpadjdf adsfouaobjfad fdadoubbapof adofdabsdfjasdf',
+        brand:'Apple',
+        category:'Electronics',
+        price:50.50,
+        countInStock:122,
+        rating:3.5,
+        numReviews:13
+    },
+    {
+        _id:2,
+        name:'def def def def def def def def def def def def def',
+        image:'images/airpods.jpg',
+        description:'afdadfa adfa dfoqf jad fapddfaj;dfapdfjapdf apddfj adpfdajsdfp asdpdfbapdodf asdpfouaopbbjdfadfd aosdfubapodfjapdjfdopajfdpadjdf adsfouaobjfad fdadoubbapof adofdabsdfjasdf',
+        brand:'Samsung',
+        category:'Electronics',
+        price:90.50,
+        countInStock:150,
+        rating:4.0,
+        numReviews:20
+    },
+    {
+        _id:3,
+        name:'xyx xyx xyx xyx xyx',
+        image:'images/airpods.jpg',
+        description:'afdadfa adfa dfoqf jad fapddfaj;dfapdfjapdf apddfj adpfdajsdfp asdpdfbapdodf asdpfouaopbbjdfadfd aosdfubapodfjapdjfdopajfdpadjdf adsfouaobjfad fdadoubbapof adofdabsdfjasdf',
+        brand:'Xiomi',
+        category:'Electronics',
+        price:70.50,
+        countInStock:100,
+        rating:4.5,
+        numReviews:25
+    },
+]
+
+export default products;
